b01e2046f0c5972648084c08a873630282d8ec14
rodolforomera <rodolforomeras@gmail.com>
2022-02-01 17:42:54 -0300

    Docker compose e importação do kafka-go

---
+++ cmd/producer/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("Hello Go")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rodolforomera/fc2-gokafka
+
+go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
