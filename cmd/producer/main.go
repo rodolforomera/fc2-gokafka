402a5d7d869e1eddd5a1ced5f8fe6ca521d40fce
rodolforomera <rodolforomeras@gmail.com>
2022-02-02 09:30:08 -0300

    Delivery report - sincrono

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -7,9 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Entrou")
+	deliveryChan := make(chan kafka.Event)
+
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+
+	e := <-deliveryChan //sincrono (Não é muito boa essa forma)
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	}else{
+		fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -24,13 +34,13 @@ func NewKafkaProducer() *kafka.Producer{
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value: []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key: key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
