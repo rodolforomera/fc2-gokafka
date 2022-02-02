66a202c5804d4eb0d7eacfe4075f085931139c1d
rodolforomera <rodolforomeras@gmail.com>
2022-02-02 10:04:30 -0300

    Delivery report - assincrono, propriedades e key

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -10,22 +10,26 @@ func main() {
 	deliveryChan := make(chan kafka.Event)
 
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil, deliveryChan)
+	Publish("transferencia", "teste", producer, []byte("transferencia2"), deliveryChan)
+	go DeliveryReport(deliveryChan) //assincrono (Outra thread)
 
-	e := <-deliveryChan //sincrono (Não é muito boa essa forma)
-	msg := e.(*kafka.Message)
-	if msg.TopicPartition.Error != nil {
-		fmt.Println("Erro ao enviar")
-	}else{
-		fmt.Println("Mensagem enviada:", msg.TopicPartition)
-	}
+	// e := <-deliveryChan //sincrono (Não é muito boa essa forma)
+	// msg := e.(*kafka.Message)
+	// if msg.TopicPartition.Error != nil {
+	// 	fmt.Println("Erro ao enviar")
+	// }else{
+	// 	fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	// }
 
-	producer.Flush(1000)
+	producer.Flush(10000)
 }
 
 func NewKafkaProducer() *kafka.Producer{
 	configMap := &kafka.ConfigMap{
 		"bootstrap.servers": "gokafka_kafka_1:9092",
+		"delivery.timeout.ms": "0",
+		"acks": "all", //0,1 ou all
+		"enable.idempotence": "true", //default: false (When true acks = all)
 	}
 	p, err := kafka.NewProducer(configMap)
 	if err != nil {
@@ -45,4 +49,19 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 		return err
 	}
 	return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar")
+			}else{
+				fmt.Println("Mensagem enviada:", ev.TopicPartition)
+				// anotar no banco de dados que a mensagem foi processada
+				// ex: confirma que uma transferencia bancaria ocorreu.
+			}	
+		}
+	}
 }
